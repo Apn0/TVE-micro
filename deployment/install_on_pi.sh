662a1dafa3ef381c0255399fe57b5bae15d9c5cc
Apn0 <arno13579@gmail.com>
2025-11-25 01:51:00 +0100

    Add files via upload

---
+++ deployment/install_on_pi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "--- INTAREMA INSTALLER ---"
+sudo apt-get update && sudo apt-get install -y python3-pip nodejs npm git
+cd ../backend
+pip3 install -r requirements.txt --break-system-packages
+cd ../frontend
+npm install
+npm run build
+sudo cp ../deployment/intarema.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable intarema.service
+sudo systemctl restart intarema.service
+echo "Backend is running."
